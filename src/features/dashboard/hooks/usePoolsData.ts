41501c317db340d21e66091da37b6c5a95e9d3e0
Александр <55823825+Leepey@users.noreply.github.com>
2025-09-24 21:36:11 +1000

    Delete src directory

--- src/features/dashboard/hooks/usePoolsData.ts
+++
@@ -1,8 +0,0 @@
-// src/features/dashboard/hooks/usePoolsData.ts
-import { useMixerContext } from '../../../context/MixerContext';
-import type { Pool } from '../types/dashboard.types';
-
-export const usePoolsData = (): Pool[] => {
-  const { availablePools } = useMixerContext();
-  return availablePools;
-};
