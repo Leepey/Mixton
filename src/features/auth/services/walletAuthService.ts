b8a7da21677e1f6ea6cfe9c60f3356231ecca5aa
Александр <55823825+Leepey@users.noreply.github.com>
2025-09-25 18:34:20 +1000

    Delete src directory

--- src/features/auth/services/walletAuthService.ts
+++
@@ -1,6 +0,0 @@
-// src/features/auth/services/walletAuthService.ts
-export const walletAuthService = {
-  connectWallet: async () => { /* ... */ },
-  disconnectWallet: async () => { /* ... */ },
-  verifyWallet: async (address) => { /* ... */ },
-};
