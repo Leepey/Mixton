026672646c5d753ccdc02a9897bf1d5390a25e75
Leepey <leepey@mail.ru>
2025-09-27 14:53:36 +1000

    del

--- src/features/admin/components/index.ts
+++
@@ -1,9 +0,0 @@
-// features/admin/components/index.ts
-export { AdminPanelLayout } from './AdminPanelLayout';
-export { AnalyticsPanel } from './AnalyticsPanel';
-export { ContractSettings } from '../../about/components/ContractSettings';
-export { ContractSettingsComponent } from './ContractSettingsComponent';
-export { SecuritySettingsComponent } from './SecuritySettingsComponent';
-export { UserManagement } from './UserManagement';
-export { TransactionTable } from './TransactionTable';
-export { AdminTransactionDetails } from './AdminTransactionDetails';
