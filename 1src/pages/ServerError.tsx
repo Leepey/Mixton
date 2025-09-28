d5d685e32a41d5d5186a22a4b30699ac76fedcef
Александр <55823825+Leepey@users.noreply.github.com>
2025-09-28 10:28:27 +1000

    Delete 1src directory

--- 1src/pages/ServerError.tsx
+++
@@ -1,9 +0,0 @@
-// pages/ServerError.tsx
-import React from 'react';
-import { ServerErrorPage } from '../features/shared/components/errors/ServerErrorPage';
-
-const ServerError: React.FC = () => {
-  return <ServerErrorPage />;
-};
-
-export default ServerError;
