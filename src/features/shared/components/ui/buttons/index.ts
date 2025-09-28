2bd5a4d48e143190188cc5247b78d3b4cd204c9f
Александр <55823825+Leepey@users.noreply.github.com>
2025-09-28 10:28:46 +1000

    Delete src directory

--- src/features/shared/components/ui/buttons/index.ts
+++
@@ -1,4 +0,0 @@
-// src/features/shared/components/ui/buttons/index.ts
-export { MixButton, MixButtonPresets } from './MixButton';
-export { TonConnectButton, TonConnectButtonPresets } from './TonConnectButton';
-export { NeoButton, NeoButtonPresets } from './NeoButton';
